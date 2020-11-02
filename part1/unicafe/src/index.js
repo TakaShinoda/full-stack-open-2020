86dcf2ecd744b01f228662b51b9a0dd810bfde93
Takahiro <tkhr_621@docomo.ne.jp>
2020-11-02 22:55:31 +0900

    fix code style

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -2,21 +2,19 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const Statistics = ({ good, neutral, bad, all, average, positive }) => {
-  // console.log(props)
-  return(
+  return (
     <>
-  <p>good {good}</p>
-  <p>neutral {neutral}</p>
-  <p>bad {bad}</p>
-  <p>all {all}</p>
-  <p>average {average}</p>
-  <p>positive {positive}</p>
-  </>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive}</p>
+    </>
   )
 }
 
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
