36c80b78619a481f6af02f4cb41a28e09d8f76d7
Takahiro <tkhr_621@docomo.ne.jp>
2020-11-02 22:54:26 +0900

    演習1.8

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,6 +1,20 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Statistics = ({ good, neutral, bad, all, average, positive }) => {
+  // console.log(props)
+  return(
+    <>
+  <p>good {good}</p>
+  <p>neutral {neutral}</p>
+  <p>bad {bad}</p>
+  <p>all {all}</p>
+  <p>average {average}</p>
+  <p>positive {positive}</p>
+  </>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -9,22 +23,26 @@ const App = () => {
 
   const all = good + neutral + bad
   const average = all ? (good * 1 + neutral * 0 + bad * -1) / all : 0
-  const positive = all ? `${good / all}%` : "0%"
+  const positive = all ? `${good / all}%` : '0%'
 
   return (
     <>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(prevGood => prevGood + 1)}>good</button>
-      <button onClick={() => setNeutral(prevNeutral => prevNeutral + 1)}>neutral</button>
-      <button onClick={() => setBad(prevaBad => prevaBad + 1)}>bad</button>
+      <button onClick={() => setGood((prevGood) => prevGood + 1)}>good</button>
+      <button onClick={() => setNeutral((prevNeutral) => prevNeutral + 1)}>
+        neutral
+      </button>
+      <button onClick={() => setBad((prevaBad) => prevaBad + 1)}>bad</button>
 
       <h2>statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {positive}</p>
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        all={all}
+        average={average}
+        positive={positive}
+      />
     </>
   )
 }
